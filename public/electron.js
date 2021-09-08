dd4e584da0a453e9bedce363a60ec65da71de75f
antoniogiordano <antonio.giordano@getapper.com>
2021-09-08 16:39:25 +0200

    open dialog working

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,5 @@
 const electron = require("electron");
+const { dialog, ipcMain } = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
@@ -9,23 +10,42 @@ let mainWindow;
 const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 900,
-    height: 680
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
   });
   /**
    * Controllo di versione
    */
   mainWindow.loadURL(
-    isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`,
   );
-  mainWindow.on("closed", () => (
-    mainWindow = null
-  ))
-}
+  mainWindow.on("closed", () => (mainWindow = null));
 
-app.on("ready", createWindow)
+  ipcMain.on("invokeAction", (event, arg) => {
+    dialog
+      .showOpenDialog(mainWindow, {
+        properties: ["openFile", "openDirectory"],
+      })
+      .then((result) => {
+        console.log(result.canceled);
+        console.log(result.filePaths);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    event.reply("actionReply", "pong");
+  });
+};
+
+app.on("ready", createWindow);
 app.on("window-all-closed", () => {
-  process.platform !== "darwin" && app.quit()
-})
+  process.platform !== "darwin" && app.quit();
+});
 app.on("activate", () => {
-  mainWindow === null && createWindow()
-})
+  mainWindow === null && createWindow();
+});
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,17 +1,32 @@
-import React, { memo } from "react";
-import { MuiThemeProvider } from "@material-ui/core";
+import React, { memo, useEffect } from "react";
+import { Button, MuiThemeProvider } from "@material-ui/core";
 import { Switch, Route } from "react-router-dom";
 import AppSnackbar from "components/AppSnackbar";
 import useAppHooks from "./index.hooks";
+const { ipcRenderer } = window.require("electron");
 
 const App: React.FC = () => {
   const { theme } = useAppHooks();
+
+  useEffect(() => {
+    ipcRenderer.once("actionReply", function (event, response) {
+      alert(JSON.stringify(response));
+    });
+  }, []);
 
   return (
     <MuiThemeProvider theme={theme}>
       <Switch>
         <Route path="/">
-          <div>Test message ok</div>
+          <div>
+            <Button
+              onClick={() => {
+                ipcRenderer.send("invokeAction", "someData");
+              }}
+            >
+              Open
+            </Button>
+          </div>
         </Route>
       </Switch>
       <AppSnackbar />
